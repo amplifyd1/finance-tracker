6fe16f64d3e092461b5ab16e7bb79bf62641a3ce
BaseballApp <wesleysprague@yahoo.com>
2018-01-30 02:28:14 +0000

    added whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  config.web_console.whitelisted_ips = '10.0.2.2'
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 end
